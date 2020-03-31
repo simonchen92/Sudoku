524ece7b45744e6dcc021015947f13d38fc58c61
simonchen92 <simon.k.chen92@gmail.com>
2020-03-31 17:07:27 -0400

    Changed naming convention for board

--- working.py
+++ working.py
@@ -11,18 +11,18 @@ board = [
 ]
 
 # Visual output of board
-def print_board(bo):
-    for i in range(len(bo)):
+def print_board(board):
+    for i in range(len(board)):
         if i % 3 == 0 and i != 0:
             print("- - - - - - - - - - - - - ")
 
-        for j in range(len(bo[0])):
+        for j in range(len(board[0])):
             if j % 3 == 0 and j != 0:
                 print(" | ", end="")
 
             if j == 8:
-                print(bo[i][j])
+                print(board[i][j])
             else:
-                print(str(bo[i][j]) + " ", end="")
+                print(str(board[i][j]) + " ", end="")
 
 print_board(board)
